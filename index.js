fe53b15ddf85374bf2d7b5a7cdaf94fbed360aab
Petunia675 <ogekepurity675@gmail.com>
2024-05-11 09:20:20 +0300

    functional game logic

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ function cellClicked(cellIndex) {
             message = `player o, your turn`
         }
         playerTurnEl.innerHTML = message
+        checkWin()
     }
     playerTurnEl.classList.remove('player-X');
     playerTurnEl.classList.remove('player-O');
@@ -66,16 +67,31 @@ const winningConditions = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
+    [0, 3, 6],
     [1, 4, 7],
-    [2, 5, 6],
+    [2, 5, 8],
     [0, 4, 8],
-    [0, 1, 2],
     [2, 4, 6]
 ];
-
+function checkWin() {
+    for (let i = 0; i < winningConditions.length; i++) {
+      const [a, b, c] = winningConditions[i];
+      if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
+        playerTurnEl.innerHTML = `Player ${gameBoard[a]} wins!`;
+        gameActive = false;
+        return;
+      }
+    }
+    if (!gameBoard.includes('')) {
+      playerTurnEl.innerHTML = 'It\'s a draw!';
+      gameActive = false;
+    }
+    
+  }
 //EVENTS
 restartBtn.addEventListener("click", function() {
     clear()
     bgimage()
+    gameActive = true;
  })
  
